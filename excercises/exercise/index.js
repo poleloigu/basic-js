87eaa3e3a5ebca4bb77911c11d42ec4952eaa549
Ole Loigu <ole.loigu@avalanchelabs.ee>
2020-10-31 09:43:24 +0200

    commit

--- excercises/exercise/index.js
+++ excercises/exercise/index.js
@@ -1,4 +1,4 @@
-console.log('Working');
+// console.log('Working');
 
 // * Adding JS to your HTML
 // We will be doing this in the .html file itself.
@@ -13,15 +13,43 @@ Here aswell
 
 // * Devtools & logging
 // extra features to check out: console.group(), console.error()
-console.log('String');
-console.log(10);
-alert('String');
-alert(15);
+// console.log('String');
+// console.log(10);
+// alert('String');
+// alert(15);
 document.write('String');
 document.write(10);
 
-// Variables
-// Variable types (simple, complex, special)
+// Variables (If i type something in all caps, that means you can replace it with anything you want)
+// extra features: const, let;
+var NAME;
+// console.log(NAME);
+NAME = 'Value'; // assinging variable a value;
+// name can have letters, numbers and _
+// cant start with a number
+// case sensitive
+// some keywords are reserved (var, function)
+
+// Variable types (simple, complex, special)  JS IS DYNAMICALLY TYPED
+var string = 'string or any type of text'; // string
+var number = 10;
+var float = 10.55;
+var boolean = true; // can be false as well;
+// console.log(typeof float); // typeof gives you the type
+var array = [1, 2, 3, 4];
+var object = {
+	name: 'Ole',
+	age: 25,
+	greet: function () {
+		alert('greetings');
+	},
+};
+
+var noValue = null;
+var notANumber = NaN;
+var undef = undefined;  // var undef;
+// String operations
+
 
 // Math operations
 
