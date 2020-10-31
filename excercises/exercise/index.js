128c7e3c9110c895588082b4fcb9e29f9eaae609
Ole Loigu <ole.loigu@avalanchelabs.ee>
2020-10-31 08:35:50 +0200

    commit

--- excercises/exercise/index.js
+++ excercises/exercise/index.js
@@ -1,1 +1,24 @@
 console.log('Working');
+
+// Adding JS to your HTML
+
+// Comments
+
+// Devtools
+
+// Variables
+// Variable types (simple, complex, special)
+
+// Math operations
+
+// Comparison
+
+// Conditionals
+
+// Functions
+
+// Loops
+
+// Arrays
+
+// Objects
